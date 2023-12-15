1dec0f9e8e0034a6b403a1e9c1fc1827f1bb2418
Luke Marsden <me@lukemarsden.net>
2023-12-15 19:56:56 +0000

    Don't leave axolotl fine-tuning running forever

--- src/axolotl/cli/train.py
+++ src/axolotl/cli/train.py
@@ -42,45 +42,45 @@ def do_cli(config: Path = Path("examples/"), **kwargs):
     if getJobURL is None:
         sys.exit("HELIX_GET_JOB_URL is not set")
 
-    while True:
-        response = requests.get(getJobURL)
-        if response.status_code != 200:
-            time.sleep(0.1)
-            continue
+    response = requests.get(getJobURL)
+    if response.status_code != 200:
+        time.sleep(0.1)
+        # TODO: retry?
+        return
 
-        task = json.loads(response.content)
+    task = json.loads(response.content)
 
-        print("🟣 Mistral Job --------------------------------------------------\n")
-        print(task)
+    print("🟣 Mistral Job --------------------------------------------------\n")
+    print(task)
 
-        session_id = task["session_id"]
-        dataset_dir = task["dataset_dir"]
+    session_id = task["session_id"]
+    dataset_dir = task["dataset_dir"]
 
-        results_dir = f"/tmp/helix/results/{session_id}"
-        
-        Path(results_dir).mkdir(parents=True, exist_ok=True)
+    results_dir = f"/tmp/helix/results/{session_id}"
+    
+    Path(results_dir).mkdir(parents=True, exist_ok=True)
 
-        # pylint: disable=duplicate-code
-        parsed_cfg = load_cfg(config, **kwargs)
+    # pylint: disable=duplicate-code
+    parsed_cfg = load_cfg(config, **kwargs)
 
-        parsed_cfg["datasets"][0]["path"] = f"{dataset_dir}/finetune_dataset.jsonl"
-        parsed_cfg["datasets"][0]["type"] = "sharegpt"
-        parsed_cfg["datasets"][0]["ds_type"] = "json"
-        parsed_cfg["output_dir"] = results_dir
+    parsed_cfg["datasets"][0]["path"] = f"{dataset_dir}/finetune_dataset.jsonl"
+    parsed_cfg["datasets"][0]["type"] = "sharegpt"
+    parsed_cfg["datasets"][0]["ds_type"] = "json"
+    parsed_cfg["output_dir"] = results_dir
 
-        import pprint; pprint.pprint(parsed_cfg)
+    import pprint; pprint.pprint(parsed_cfg)
 
-        check_accelerate_default_config()
-        check_user_token()
-        parser = transformers.HfArgumentParser((TrainerCliArgs))
-        parsed_cli_args, _ = parser.parse_args_into_dataclasses(
-            return_remaining_strings=True
-        )
-        print(f"[SESSION_START]session_id={session_id}", file=sys.stdout)
-        dataset_meta = load_datasets(cfg=parsed_cfg, cli_args=parsed_cli_args)
-        train(cfg=parsed_cfg, cli_args=parsed_cli_args, dataset_meta=dataset_meta)
-        remove_folders_with_prefix(results_dir, "checkpoint")
-        print(f"[SESSION_END_LORA_DIR]lora_dir={results_dir}", file=sys.stdout)
+    check_accelerate_default_config()
+    check_user_token()
+    parser = transformers.HfArgumentParser((TrainerCliArgs))
+    parsed_cli_args, _ = parser.parse_args_into_dataclasses(
+        return_remaining_strings=True
+    )
+    print(f"[SESSION_START]session_id={session_id}", file=sys.stdout)
+    dataset_meta = load_datasets(cfg=parsed_cfg, cli_args=parsed_cli_args)
+    train(cfg=parsed_cfg, cli_args=parsed_cli_args, dataset_meta=dataset_meta)
+    remove_folders_with_prefix(results_dir, "checkpoint")
+    print(f"[SESSION_END_LORA_DIR]lora_dir={results_dir}", file=sys.stdout)
 
 if __name__ == "__main__":
     fire.Fire(do_cli)
